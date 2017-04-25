3f2918b1dc72442172bb990c9cf3239a5b80b80b
nicolas.pena <nicolaspmcg@gmail.com>
2017-04-25 20:11:13 -0300

    2

---
+++ museo/js/app.js
@@ -0,0 +1,17 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+/*var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+	var scrolled = $(window).scrollTop();
+	$('.jumbotron-2').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+	parallax();
+});*/
